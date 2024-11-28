0f43594f43cfb4d80718d24198f01a2d42c39f83
Mithun@2303 <mithunkarthick1610@gmail.com>
2024-11-28 12:53:07 +0530

    Login

---
+++ src/constants/constants.jsx
@@ -0,0 +1,12 @@
+export  const Host = {
+    HOST: "http://192.168.105.17:5173/",
+    APIHOST:"http://192.168.105.17:8080/"
+}
+
+export  const ApiRoutes={
+    LOGIN:"login"
+}
+
+export const ReactRoutes = {
+    LOGIN:"/"
+}
---
+++ src/pages/Login.jsx
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { useState } from "react"
+import { ApiRoutes, Host } from "../constants/constants";
+
+
+
+const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [errMsg, setErrMsg] = useState("");
+    const [showErr, setShowErr] = useState(false);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (email.length != 0 && password.length != 0) {
+            try {
+                const response = await axios.get(Host.APIHOST + ApiRoutes.LOGIN + `?email=${email}&password=${password}/`);
+                if (response.status == 200) {
+                    setErrMsg("Valid Credentials");
+            setShowErr(true);
+
+                }
+            } catch (error) {
+                setErrMsg(error.response)
+            setShowErr(true);
+
+            }
+
+        }
+        else{
+            setErrMsg("Email and Password is required");
+            setShowErr(true);
+        }
+    }
+    return (
+        <div className="w-screen h-screen bg-white  text-xl flex flex-col justify-center items-center">
+            <div className="flex flex-col justify-start space-y-5">
+                <div className="flex flex-col">
+
+                    <label htmlFor="Email"> Email</label>
+                    <input id="Email" type="text" 
+                    className="bg-white rounded-2xl p-3 border-2 border-black" 
+                    onChange={(e)=>setEmail(e.target.value)}/>
+                </div>
+                <div className="flex flex-col">
+
+                    <label htmlFor="password"> Password</label>
+                    <input id="password" type="text" className="bg-white rounded-2xl p-3 border-2 border-black" 
+                    onChange={(e)=>setPassword(e.target.value)}/>
+
+                </div>
+                <button className="p-3 w-full bg-red-500 rounded-2xl"
+                onClick={(e)=>handleSubmit(e)}>Submit</button>
+                {showErr && <p className="text-black w-fit">{errMsg}</p>}
+            </div>
+
+        </div>
+    )
+}
+
+export default Login
